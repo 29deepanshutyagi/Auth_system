1d8ca446ce056eeebe3dcd3971eafeb1cf162ca1
29deepanshutyagi <123951725+29deepanshutyagi@users.noreply.github.com>
2023-11-21 02:21:40 +0530

    updates

---
+++ frontend/src/store.js
@@ -0,0 +1,28 @@
+
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+// import rootReducer from './reducers';
+
+// const initialState = {};
+
+// const middleware = [thunk];
+
+// const store = createStore(
+//     rootReducer,
+//     initialState,
+//     composeWithDevTools(applyMiddleware(...middleware))
+// );
+
+// export default store;
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
+const initialState = {};
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk],
+});
+
+export default store;
+
+
